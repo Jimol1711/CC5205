ace764b6fc33fd5fc07ce37f558022a81a7ca6d0
Jimol1711 <juan.molina.l@ug.uchile.cl>
2024-04-28 14:28:52 -0400

    "testing venv"

--- proyecto_mineria/anime_reader.py
+++ proyecto_mineria/anime_reader.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
-users = pd.read_csv('users_cleaned.csv')
-anime = pd.read_csv('anime_cleaned.csv')
-anime_lists = pd.read_csv('animelists_cleaned.csv')
+users = pd.read_csv('/archive/users_cleaned.csv')
+anime = pd.read_csv('/archive/anime_cleaned.csv')
+# anime_lists = pd.read_csv('animelists_cleaned.csv')
 
 print(users.head())
